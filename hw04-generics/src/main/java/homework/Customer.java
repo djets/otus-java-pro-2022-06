f2d16ea373e378a8dd697fa2fcaf7eb0f0f9b23a
djets <jag0s3@gmail.com>
2022-08-09 10:20:57 +0300

    hw-04. reverseOrderTest-passed

---
+++ hw04-generics/src/main/java/homework/Customer.java
@@ -0,0 +1,64 @@
+package homework;
+
+import java.util.Objects;
+
+public class Customer {
+    private final long id;
+    private String name;
+    private long scores;
+
+    //todo: 1. в этом классе надо исправить ошибки
+
+    public Customer(long id, String name, long scores) {
+        this.id = id;
+        this.name = name;
+        this.scores = scores;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getScores() {
+        return scores;
+    }
+
+    public void setScores(long scores) {
+        this.scores = scores;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", scores=" + scores +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer customer)) return false;
+
+        if (id != customer.id) return false;
+        if (scores != customer.scores) return false;
+        return Objects.equals(name, customer.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (int) (scores ^ (scores >>> 32));
+        return result;
+    }
+}
