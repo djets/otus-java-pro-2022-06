b4ec7a60e42cacb118e84c0e29f8eb6f2012799f
djets <jag0s3@gmail.com>
2022-09-04 22:34:54 +0300

    hw-06. Refactoring.

--- hw06-annotations/src/main/java/Runner.java
+++ hw06-annotations/src/main/java/Runner.java
@@ -1,11 +1,14 @@
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Runner {
 
     public static void main(String[] args) {
-        var instanceSomeTestClass = instantiation("SomeTestClass");
-        runTest(instanceSomeTestClass);
+        String nameTestClass = "SomeTestClass";
+        runTest(getDeclaredMethodMarkedAnnotations(nameTestClass), nameTestClass);
     }
 
     private static Object instantiation(String nameTestClass) {
@@ -17,51 +20,65 @@ public class Runner {
         }
     }
 
-    private static void runBefore(Object someTestClass) {
-        for (Method method : someTestClass.getClass().getDeclaredMethods()) {
-            if (method.isAnnotationPresent(Before.class)) {
-                try {
-                    method.setAccessible(true);
-                    System.out.println(method.invoke(someTestClass));
-                } catch (InvocationTargetException | IllegalAccessException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
+    private static List<Method> getDeclaredMethodMarkedAnnotations(String nameTestClass) {
+        try {
+            return Arrays.stream(Class.forName(nameTestClass).getDeclaredMethods())
+                    .filter(method -> method.getDeclaredAnnotations().length > 0).collect(Collectors.toList());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Class not found");
         }
     }
 
-    private static void runTest(Object someTestClass) {
+    private static void runBefore(List<Method> getDeclaredMethodMarkedAnnotations, Object instanceTestClass) {
+        getDeclaredMethodMarkedAnnotations.stream().filter(method -> method.isAnnotationPresent(Before.class))
+                .forEach(method -> {
+                    try {
+                        method.setAccessible(true);
+                        System.out.println(method.invoke(instanceTestClass));
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e.getCause());
+                    } catch (InvocationTargetException e) {
+                        throw new RuntimeException(e.getCause());
+                    } finally {
+                        runAfter(getDeclaredMethodMarkedAnnotations, instanceTestClass);
+                    }
+                });
+    }
+
+    private static void runTest(List<Method> getDeclaredMethodMarkedAnnotations, String nameTestClass) {
         int tests = 0;
         int testPassed = 0;
 
-        for (Method method : someTestClass.getClass().getDeclaredMethods()) {
-            if (method.isAnnotationPresent(Test.class)) {
-                tests++;
-                try {
-                    runBefore(someTestClass);
-                    method.setAccessible(true);
-                    method.invoke(someTestClass);
-                    System.out.printf("Running Test. Method - %s%n", method.getName());
-                    testPassed++;
-                    runAfter(someTestClass);
-                } catch (InvocationTargetException | IllegalAccessException e) {
-                    System.out.println(method + " failed: " + e.getCause());
-                }
+        for (Method method : getDeclaredMethodMarkedAnnotations
+                .stream().filter(method -> method.isAnnotationPresent(Test.class))
+                .collect(Collectors.toList())) {
+            tests++;
+            try {
+                Object instanceTestClass = instantiation(nameTestClass);
+                runBefore(getDeclaredMethodMarkedAnnotations, instanceTestClass);
+                method.setAccessible(true);
+                method.invoke(instanceTestClass);
+                System.out.printf("Running Test. Method - %s%n", method.getName());
+                testPassed++;
+            } catch (RuntimeException | InvocationTargetException | IllegalAccessException e) {
+                System.out.println(method + " failed: " + e.getCause());
             }
         }
+
         System.out.printf("Passed: %d, Failed: %d%n", testPassed, tests - testPassed);
     }
 
-    private static void runAfter(Object someTestClass) {
-        for (Method method : someTestClass.getClass().getDeclaredMethods()) {
-            if (method.isAnnotationPresent(After.class)) {
-                try {
-                    method.setAccessible(true);
-                    System.out.println(method.invoke(someTestClass));
-                } catch (InvocationTargetException | IllegalAccessException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-        }
+    private static void runAfter(List<Method> getDeclaredMethodMarkedAnnotations, Object instanceTestClass) {
+        getDeclaredMethodMarkedAnnotations.stream().filter(method -> method.isAnnotationPresent(After.class))
+                .forEach(method -> {
+                    try {
+                        method.setAccessible(true);
+                        System.out.println(method.invoke(instanceTestClass));
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e.getCause());
+                    } catch (InvocationTargetException e) {
+                        throw new RuntimeException(e.getCause());
+                    }
+                });
     }
 }
