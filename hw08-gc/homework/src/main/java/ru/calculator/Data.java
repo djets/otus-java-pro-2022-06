48aef0da5e704d8cb31eb5abb3b1b5d2fb1d41f7
djets <jag0s3@gmail.com>
2022-09-14 11:26:56 +0300

    hw-08.

---
+++ hw08-gc/homework/src/main/java/ru/calculator/Data.java
@@ -0,0 +1,13 @@
+package java.ru.calculator;
+
+public class Data {
+    private final Integer value;
+
+    public Data(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
