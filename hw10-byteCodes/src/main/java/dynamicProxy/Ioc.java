c425b3334985acff7f997389fba1d226541741fd
djets <jag0s3@gmail.com>
2022-09-24 11:34:28 +0300

    hw-10. Minor fix: performance

--- hw10-byteCodes/src/main/java/dynamicProxy/Ioc.java
+++ hw10-byteCodes/src/main/java/dynamicProxy/Ioc.java
@@ -1,25 +1,25 @@
 package dynamicProxy;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-
+import java.util.*;
 public class Ioc {
-
-    public Ioc() {
+    private Ioc() {
     }
 
     static TestLoggingInterface createMyClass(TestLoggingInterface myClass) {
+        Map<Method, Annotation> contextMethodListAnnotated = new HashMap<>();
         return (TestLoggingInterface) Proxy.newProxyInstance(myClass.getClass().getClassLoader(),
                 new Class[]{TestLoggingInterface.class}, (Object proxy, Method method, Object[] args) -> {
-                    if (myClass.getClass().getMethod(method.getName(), method.getParameterTypes()).isAnnotationPresent(LogScam.class)) {
-                        System.out.printf("executed method: %s, param: ", method.getName());
+                    if (contextMethodListAnnotated.containsKey(method)) {
+                        System.out.printf("executed method from context: %s, param: ", method.getName());
+                    } else {
+                        if (myClass.getClass().getMethod(method.getName(), method.getParameterTypes()).isAnnotationPresent(LogScam.class)) {
+                            contextMethodListAnnotated.putIfAbsent(method, method.getAnnotation(LogScam.class));
+                            System.out.printf("executed method: %s, param: ", method.getName());
+                        }
                     }
                     return method.invoke(myClass, args);
                 });
     }
-
-
 }
