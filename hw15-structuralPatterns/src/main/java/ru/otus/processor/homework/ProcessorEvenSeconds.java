67d51e5c82c781bc1848cd4cbda0a751c431e00c
djets <jag0s3@gmail.com>
2022-10-03 08:31:14 +0300

    hw-15. Second fix.

--- hw15-structuralPatterns/src/main/java/ru/otus/processor/homework/ProcessorEvenSeconds.java
+++ hw15-structuralPatterns/src/main/java/ru/otus/processor/homework/ProcessorEvenSeconds.java
@@ -3,21 +3,20 @@ package ru.otus.processor.homework;
 import ru.otus.model.Message;
 import ru.otus.processor.Processor;
 
-import java.sql.Time;
-import java.util.*;
+import java.time.LocalTime;
 
 public class ProcessorEvenSeconds implements Processor {
     long evenSecond;
     public ProcessorEvenSeconds() {
-        evenSecond = new Date().getTime();
+        evenSecond = LocalTime.now().getSecond();
     }
     public ProcessorEvenSeconds(long setTime) {
         this.evenSecond = setTime;
     }
     @Override
     public Message process(Message message) {
-        if ((evenSecond / 60) % 2 == 0) {
-            throw new RuntimeException("Четная");
+        if (evenSecond % 2 == 0) {
+            throw new RuntimeException("Четная секунда");
         }
         return null;
     }
